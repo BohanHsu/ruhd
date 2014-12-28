545d373f8c707d10a88955b54234c55653945728
Bohan <bohanhsu1990@gmail.com>
2014-12-28 20:46:47 +0800

    issue-4: update model index file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var _ = require('underscore')
 
-_.each(['user'], function (name) {
+_.each(['user', 'role', 'user-has-role'], function (name) {
   var model = require('./' + name)
 
   module.exports[model.modelName] = model
