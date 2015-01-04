b7727056cbd4a85591654e5fc813bfe57527835b
Bohan <bohanhsu1990@gmail.com>
2015-01-04 20:21:42 +0800

    issue-7: add views dir

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var app = express()
 
 require('./config/mongodb-connection')()
 
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, '/views'))
+
 app.use('/vendor', express.static(path.join(__dirname, '/public/vendor')))
 app.use('/js', express.static(path.join(__dirname, '/public/js')))
 app.use('/css', express.static(path.join(__dirname, '/public/css')))
